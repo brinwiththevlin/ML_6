462dfe80c3524123c9227e9adf39e67344aec9ef
Bryan Cora <corabj@mail.uc.edu>
2023-11-28 15:41:59 -0500

    ignore pics

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ploting as plot
 import nueralnet as NN
 import numpy as np
+import time
 
 if __name__ == "__main__":
     np.random.seed(5280)
@@ -9,11 +10,14 @@ if __name__ == "__main__":
 
     # part 1
     for style in ["incremental", "batch"]:
+        start = time.time()
         for lrate in [0.5, 0.25, 0.1, 1e-2, 1e-3, 1e-4]:
             print(f"style: {style}, lrate: {lrate}")
             model = NN.unitNN(2, 1)
             errors, planes = model.train(X, Y, style, lrate)
-            print("plotting")
+            print("plotting error")
             plot.plot_error(errors, style, lrate)
-            print("plotting again")
+            print("plotting boundaries")
             plot.plot_decision_planes(X, Y, planes, style, lrate)
+        end = time.time()
+        print(f"total time spend on {style}: {end - start}")
